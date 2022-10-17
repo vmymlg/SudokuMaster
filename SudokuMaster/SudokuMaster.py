88244b75b81ae2cd8870e6712b817d906d7abea5
vmymlg <qd@live.ca>
2022-10-17 15:28:29 -0400

    Start button ajouter

--- SudokuMaster/SudokuMaster.py
+++ SudokuMaster/SudokuMaster.py
@@ -11,6 +11,8 @@ from pynput import keyboard
 
 button1 = False
 button2 = False
+start_haut = False
+start_bas = False
 point_hg_x = 0
 point_hg_y = 0
 point_bd_x = 0
@@ -26,16 +28,30 @@ root.iconbitmap('sudoku_icon.ico')
 
 root.columnconfigure(0,weight=1)
 root.columnconfigure(1,weight=3)
+root.columnconfigure(2,weight=3)
 
 
 def mouse_pointer_1():
     global button1
+    global start_haut
+    start_haut = True
     button1 = True
     
 def mouse_pointer_2():
     global button2
+    global start_bas
+    start_bas = True
     button2 = True  
 
+def start_screenshot():
+    if start_haut and start_bas:
+        if point_hg_x<point_bd_x and point_hg_y<point_bd_y:
+            create_screenshot()
+        
+def create_screenshot():
+   image = pyautogui.screenshot(region=(point_hg_x,point_hg_y,point_bd_x-point_hg_x,point_bd_y-point_hg_y))
+
+start_screen = ttk.Button(root,command=start_screenshot,text="Start Screenshot")
 mouse_button_1 = ttk.Button(root,command=mouse_pointer_1,text="Haut-Gauche")
 mouse_button_2 = ttk.Button(root,command=mouse_pointer_2,text="Bas-Droit")
 label_pointer1 = ttk.Label(root,text="Pointeur haut-gauche tableau")
@@ -45,22 +61,29 @@ label_pointer1.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 label_pointer2.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
 mouse_button_1.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
 mouse_button_2.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
-
+start_screen.grid(column=2, row=0, sticky=tk.W, padx=5, pady=5)
 def on_press(key):
    global button1
    global button2
+   global point_hg_x
+   global point_hg_y
+   global point_bd_x
+   global point_bd_y
+   if key == keyboard.Key.esc:
+       listener.stop()
+       root.destroy()
    if button1:
        mouse_position = pyautogui.position()
        point_hg_x = mouse_position[0]
        point_hg_y = mouse_position[1]
-       label_pointer1.config(text=( "Coordonnées:"+str(point_hg_x)+","+str(point_hg_y)))
+       label_pointer1.config(text=( "Coordonnees:"+str(point_hg_x)+","+str(point_hg_y)))
        button1 = False
     
    elif button2:
        mouse_position = pyautogui.position()
        point_bd_x = mouse_position[0]
        point_bd_y = mouse_position[1]
-       label_pointer2.config(text = "Coordonnées:"+str(point_bd_x)+","+str(point_bd_y))
+       label_pointer2.config(text = "Coordonnees:"+str(point_bd_x)+","+str(point_bd_y))
        button2 = False
        
 
