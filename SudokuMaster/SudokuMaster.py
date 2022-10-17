a4c688b8adb33f89052b2e77077b9303b79c0b45
vmymlg <qd@live.ca>
2022-10-17 12:55:28 -0400

    Add project files.

---
+++ SudokuMaster/SudokuMaster.py
@@ -0,0 +1,17 @@
+from tokenize import ContStr
+from turtle import window_height, window_width
+import tabula
+import cv2 as cv
+import pyautogui
+import tkinter as tk
+window_height = 600
+window_width = 400
+root = tk.Tk()
+root.title('Sudoku Destroyer')
+root.geometry(str(window_height)+'x'+str(window_height)+'+50+50')
+root.resizable(False,False)
+root.iconbitmap('sudoku_icon.ico')
+root.mainloop()
+
+
+def mouse
