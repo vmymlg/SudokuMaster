9e39ff236820a4a24fc807e7f3fe09ebeebb3cd5
vmymlg <qd@live.ca>
2022-10-31 11:06:23 -0400

    Commentaire rajouter

--- SudokuMaster/SudokuMaster.py
+++ SudokuMaster/SudokuMaster.py
@@ -13,10 +13,9 @@ from PIL import ImageTk,Image
 import numpy
 import time
 from threading import Thread, main_thread
-from matplotlib import image, pyplot
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from matplotlib import image as mpimg, pyplot as plt
 
+#Les variables globales sont deconseillers mais on apprends en pratiquant
 grid_sudoku = [[0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0],
@@ -26,8 +25,10 @@ grid_sudoku = [[0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0]]
+
 solution = grid_sudoku
-image_path = 'newsudoku'
+
+image_path = 'sudoku'
 vitesse_bot = 1
 #Progression Bar
 pro_nombre = False
@@ -47,9 +48,11 @@ point_hg_x = 0
 point_hg_y = 0
 point_bd_x = 0
 point_bd_y = 0
-
+#--------------------------------------------------------------
+#Demarrer la fenetre de l'application
+#--------------------------------------------------------------
 window_heights = 400
-window_widths = 600
+window_widths = 700
 root = tk.Tk()
 root.title('Sudoku Destroyer')
 root.geometry(str(window_heights)+'x'+str(window_heights))
@@ -60,20 +63,22 @@ root.iconbitmap('sudoku_icon.ico')
 root.columnconfigure(0,weight=1)
 root.columnconfigure(1,weight=3)
 root.columnconfigure(2,weight=3)
+#---------------------------------------------------------------
 
-
+#Lorsqu'on appuie sur le bouton haut-gauche active la detection de onpress()
 def mouse_pointer_1():
     global button1
     global button2
     button1 = True
     button2 = False
-    
+#Lorsqu'on appuie sur le bouton bas-droit active la detection de onpress()    
 def mouse_pointer_2():
     global button1
     global button2
     button2 = True  
     button1 = False
 
+#En appuyant sur start botting lance la fonction et verifie si les coordonnes sont logiques (haut gauche en haut a gauche du sudoku)
 def start_screenshot():
     global line_list
     lines_list.clear()
@@ -87,7 +92,7 @@ def start_screenshot():
 
         
 
-        
+#Enregistre image du tableau sudoku       
 def save_screenshot():
    image = pyautogui.screenshot(region=(point_hg_x,point_hg_y,point_bd_x-point_hg_x,point_bd_y-point_hg_y))
    image.save('test.png')
@@ -97,6 +102,7 @@ def save_screenshot():
 #Attention
 #Inspiration:
 #https://www.geeksforgeeks.org/line-detection-python-opencv-houghline-method/
+#Permet d'identifier les lignes en calculant le nombre de pixels qui se suivent par rapport à l'angle teta (radiant)
 def line_identifier():
     global lines_list
     global pro_ligne
@@ -111,7 +117,7 @@ def line_identifier():
     pro_ligne = True
     progression()
 
-
+#identifie la position des nombres selon le site choisit
 def number_identifier():
     global pro_nombre
     global image_path
@@ -129,7 +135,7 @@ def number_identifier():
     progression()
     return location
 
-
+#Function principale qui appelle les autres fonctions (Main vraiment louche)
 def start():
     location = number_identifier()
     if len(list(location[0]))+len(list(location[1]))+len(list(location[2]))+len(list(location[3]))+len(list(location[4]))+len(list(location[5]))+len(list(location[6]))+len(list(location[7]))+len(list(location[8])) == 0:
@@ -138,7 +144,7 @@ def start():
     save_screenshot()
     line_identifier()
     print(len(lines_list))
-    if image_path == 'newsudoku' and len(lines_list) != 40 or image_path == 'sudoku' and len(lines_list)!=39:
+    if image_path == 'newsudoku' and len(lines_list) != 40 or image_path == 'sudoku' and (len(lines_list)<39 or len(lines_list)>40):
         tache.config(text="Trop de lignes assurez d'avoir juste le sudoku")
         return
     find_point_info()
@@ -146,7 +152,7 @@ def start():
     Thread(target = solve()).start()
     
 
-
+#Trouve les points de lignes Haut-gauche et bas droit du tableau
 def find_point_info():
     point_x_min = 10000
     point_y_min = 10000
@@ -164,7 +170,7 @@ def find_point_info():
         if y2>point_y_max : point_y_max = y2
     dimension_cell(point_x_min,point_x_max,point_y_min,point_y_max)
     
-
+#definit la largeur des case du tableau sudoku
 def dimension_cell(point_x_min,point_x_max,point_y_min,point_y_max):
     global point_hg_x
     global point_hg_y
@@ -181,7 +187,7 @@ def dimension_cell(point_x_min,point_x_max,point_y_min,point_y_max):
     progression()
 
 
-
+#Reemplit le grid avec les nombres decouvert par number_identifier()
 def create_grid(location):
     global grid_sudoku
     global pro_grid_create
@@ -194,7 +200,7 @@ def create_grid(location):
     print(numpy.matrix(grid_sudoku))
     pro_grid_create = True
     progression()
-
+#
 def inserer_nombre_grid(location,i):
     global grid_sudoku
     for loc in location :
@@ -206,17 +212,17 @@ def inserer_nombre_grid(location,i):
 #Copier entierement https://www.youtube.com/watch?v=PZJ5mjQyxR8
 def possible(row, column, number):
     global grid_sudoku
-    #Is the number appearing in the given row?
+    #Est-ce que le nombre est dans la ligne?
     for i in range(0,9):
         if grid_sudoku[row][i] == number:
             return False
 
-    #Is the number appearing in the given column?
+    #Est-ce que le nombre est dans la colonne?
     for i in range(0,9):
         if grid_sudoku[i][column] == number:
             return False
     
-    #Is the number appearing in the given square?
+    #Est-ce que le chiffre apparait dans le carré?
     x0 = (column // 3) * 3
     y0 = (row // 3) * 3
     for i in range(0,3):
@@ -225,7 +231,7 @@ def possible(row, column, number):
                 
 
     return True
-
+#fonction recursive qui s'appelle par elle-meme
 def solve():
     global grid_sudoku
     global solution
@@ -314,26 +320,30 @@ def refresh():
     tache.config(text="Veuillez entrer les coordonnees")
     label_pointer1.config(text="Haut-Gauche")
     label_pointer2.config(text="Bas-Droit")
-
+#Change pour le site de new york times
 def change_site_Ny():
     global image_path
     image_path = "newsudoku"
     print(image_path)
+#Change pour le site de 1sudoku
 def change_site_sudoku():
     global image_path
     image_path = "sudoku"
     print(image_path)
+
+#Bouton ? qui retourne de l'information
 def information():
     messagebox.showinfo(title="Information", message="Projet fait par : Yann Montminy \n Lien Site Sudoku: \n https://www.nytimes.com/puzzles/sudoku/easy \n https://1sudoku.com/fr/sudoku/facile \n Source et reference: \n https://www.youtube.com/watch?v=PZJ5mjQyxR8 \n https://www.geeksforgeeks.org/line-detection-python-opencv-houghline-method/")
 
+#Fait apparaitre une fenetre avec le tuto en image
 def tuto():
    img = mpimg.imread('tuto_image.png')
-   imgplot = plt.imshow(img)
+   plt.imshow(img)
    plt.show()
 
     
 
-
+#Menubar en haut a gauche Ajout des options et relier avec leur function(command)
 menubar = Menu(root)
 sitemenu = Menu(menubar,tearoff=0)
 infomenu = Menu(menubar, tearoff=0)
@@ -345,7 +355,7 @@ infomenu.add_command(label = "Tutoriel",command = tuto)
 menubar.add_cascade(label="?",menu=infomenu)
 root.config(menu=menubar)
 
-
+#Aujout des boutons et labels
 tache = ttk.Label(root,text="Veuillez entrer les coordonnees")
 progress = ttk.Progressbar(root,orient=HORIZONTAL,length=500,mode = 'determinate')
 start_screen = ttk.Button(root,command=start_screenshot,text="Start Botting",state = 'disabled')
@@ -354,27 +364,30 @@ mouse_button_2 = ttk.Button(root,command=mouse_pointer_2,text="Bas-Droit",state=
 restart = ttk.Button(root,command=refresh,text="Redo",state='disabled')
 label_pointer1 = ttk.Label(root,text="Pointeur haut-gauche tableau")
 
+#Ajout d ela section vitesse
 vitesse = tk.DoubleVar()
 label_pointer2 = ttk.Label(root,text="Pointeur bas-droit tableau")
 label_vitesse = ttk.Label(root,text="Vitesse du bot:")
 r_vit_min = ttk.Radiobutton(root,text='Lent',variable = vitesse, value = 1,command = change_vitesse)
 r_vit_med = ttk.Radiobutton(root,text='Moyen',variable = vitesse, value=0.25,command = change_vitesse)
-r_vit_max = ttk.Radiobutton(root,text='Hell',variable = vitesse, value=0.01,command = change_vitesse)
+r_vit_max = ttk.Radiobutton(root,text='Highway to Hell',variable = vitesse, value=0.01,command = change_vitesse)
 vitesse.set(1)
 
-
+#Ajout dans un grid des éléments tkinter
 tache.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5,columnspan = 3)
 label_pointer1.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 label_pointer2.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-mouse_button_1.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
-mouse_button_2.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
-restart.grid(column=2, row=1, sticky=tk.W, padx=5, pady=5)
-start_screen.grid(column=2, row=0, sticky=tk.W, padx=5, pady=5)
+mouse_button_1.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5,ipadx = 20,ipady = 25)
+mouse_button_2.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5,ipadx = 20,ipady = 25)
+restart.grid(column=3, row=1, sticky=tk.W, padx=5, pady=5,columnspan = 2,ipady = 25)
+start_screen.grid(column=3, row=0, sticky=tk.W, padx=5, pady=5,columnspan = 2,ipady = 25)
 label_vitesse.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
-r_vit_min.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5)
-r_vit_med.grid(column=2, row=3, sticky=tk.W, padx=5, pady=5)
+r_vit_min.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5,ipadx = 20)
+r_vit_med.grid(column=2, row=3, sticky=tk.W, padx=5, pady=5,ipadx = 20)
 r_vit_max.grid(column=3, row=3, sticky=tk.W, padx=5, pady=5)
 progress.grid(column=0, row=4, sticky=tk.W, padx=5, pady=5,columnspan = 3)
+
+#Progressebar update
 def progression():
     if pro_bot:
         progress['value'] = 100
@@ -402,7 +415,7 @@ def progression():
         progress.update_idletasks()
     
 
-
+#onkeypress pour une sortie de secours et definir lees coordonnees
 def on_press(key):
    global button1
    global button2
@@ -410,7 +423,7 @@ def on_press(key):
    global point_hg_y
    global point_bd_x
    global point_bd_y
-   print(key)
+
    if key == keyboard.Key.esc:
        listener.stop()
        root.destroy()
